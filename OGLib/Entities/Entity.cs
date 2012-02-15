2cd7951995e97aeb8bd760bfe6656f7c92a1fd47
Th3dz <erikskoglund88@gmail.com>
2012-02-15 20:13:39 +0100

    init commit

---
+++ OGLib/Entities/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace OGLib.Entities
+{
+    public class Entity
+    {
+        public Vector2 position, origin;
+        public Color color;
+        public float rotation, scale;
+        public int source;
+
+        public Entity()
+        {
+            scale = 1f;
+            color = Color.White;
+        }
+    }
+}
